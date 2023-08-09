2adf465799d226244feaef33bfcfe07c2abc7542
Gabriel <115082578+Gabriellsantana@users.noreply.github.com>
2023-08-09 11:17:02 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+# exercicios
+# questão1
+listaa = (list(range(1, 6)))
+print(listaa[0], "posição 0 ")
+print(listaa[1], "posição 1 ")
+print(listaa[2], "posição 2 ")
+print(listaa[3], "posição 3 ")
+print(listaa[4], "posição 4 ")
+
+#questão2
+listadois = (list(range(1, 6)))
+# progs.sort()
+
+#Questão3
+listaNumeros = []
+print('Informe os 5 numeros')
+for i in range(4):
+    listaNumeros.append(input('Numero ' + str(i + 1) + ':'))
+print(listaNumeros)
+
+#Questão4
+idade = []
+print("informe sua idade")
+for d in range(3)
+    idade.append(input("Idade"+str(d+1)+":"))
+    if :
+
+print(idade)
+
